62cd3e93745d12fa923e9e5d7315a49fc3a65c7a
Héctor de Anda <30514545+Hectorreto@users.noreply.github.com>
2024-07-07 16:15:55 -0600

    feat(setup): eslint config, lint staged

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const stylistic = require('@stylistic/eslint-plugin');
+
+const customized = stylistic.configs.customize({
+  arrowParens: true,
+  blockSpacing: true,
+  braceStyle: '1tbs',
+  commaDangle: 'always-multiline',
+  indent: 2,
+  jsx: true,
+  quoteProps: 'consistent-as-needed',
+  quotes: 'single',
+  semi: true,
+});
+
+module.exports = {
+  plugins: [
+    '@stylistic',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    ...customized.rules,
+    '@stylistic/multiline-ternary': ['off',
+    ],
+    '@stylistic/jsx-one-expression-per-line': ['off',
+    ],
+  },
+};
